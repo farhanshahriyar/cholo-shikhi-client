9d5459200a7064dc096dcca2bc72fd687285edd9
farhanshahriyar <farhan.abir9999@gmail.com>
2023-12-29 10:11:29 +0600

    added signin and signup with authentication

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,10 @@
-
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-4xl font-bold text-center">Welcome to LMS</p>
-      <p className="text-base text-center">Learning Management System protected page</p>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }

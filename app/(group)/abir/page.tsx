250a34ac738498e473d1b8a3251c5031652494df
farhanshahriyar <farhan.abir9999@gmail.com>
2023-12-29 09:20:00 +0600

    environment setup done

---
+++ app/(group)/abir/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+       This is a abir page inside a group folder.
+    </div>
+  )
+}
+
+export default page
---
+++ app/(group)/routea/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+       This is a route inside a group folder.
+    </div>
+  )
+}
+
+export default page

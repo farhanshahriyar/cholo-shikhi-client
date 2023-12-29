a9c7e2016e4486e4c57cd81ed30d600657e8ae7e
farhanshahriyar <farhan.abir9999@gmail.com>
2023-12-29 09:40:29 +0600

    folder setup added

--- app/(auth)/(routes)/sign-in/page.tsx
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-       This is a abir page inside a group folder.
+       This is an unprotected page signin
     </div>
   )
 }
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-4xl font-bold text-center">Welcome to LMS</p>
+      <p className="text-base text-center">Learning Management System protected page</p>
+    </div>
+  )
+}

3c174bec1f595bce6449e9dbce556da9da846152
farhanshahriyar <farhan.abir9999@gmail.com>
2024-01-02 19:18:05 +0600

    remove useRouter and added Link button

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs"
-import { usePathname, useRouter } from "next/navigation"
+import { usePathname } from "next/navigation"
 import { LogOut } from "lucide-react";
 import { Button } from "@/components/ui/button"
 import Link from "next/link";
@@ -11,7 +11,6 @@ import Link from "next/link";
 export const NavbarRoutes = () => {
     // hooks
     const pathname = usePathname();
-    const router = useRouter();
 
     const isTeacherPage = pathname?.startsWith("/teacher")
     const isPlayerPage = pathname?.includes("/chapter")
@@ -19,10 +18,12 @@ export const NavbarRoutes = () => {
         <div className="flex gap-x-2 ml-auto">
             {
                 isTeacherPage || isPlayerPage ? (
+                    <Link href="/">
                     <Button size="sm" variant="ghost" className="hover:underline">
                         <LogOut className="h-4 w-4 mr-2"/>
                          Exit
                     </Button>
+                </Link>
                 ) : ( 
                     <Link href="/teacher/courses">
                         <Button size="sm" variant="ghost" className="hover:underline">

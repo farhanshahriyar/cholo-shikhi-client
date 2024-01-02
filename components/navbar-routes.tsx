998fdfb9de23acc635de65bc7cdf31a7be8c2adf
farhanshahriyar <farhan.abir9999@gmail.com>
2024-01-02 19:08:44 +0600

    added dynamic layout for navbar and condition for teacherPage and playerPage

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,12 +1,39 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs"
+import { usePathname, useRouter } from "next/navigation"
+import { LogOut } from "lucide-react";
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+
 
 
 export const NavbarRoutes = () => {
+    // hooks
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const isTeacherPage = pathname?.startsWith("/teacher")
+    const isPlayerPage = pathname?.includes("/chapter")
     return (
         <div className="flex gap-x-2 ml-auto">
-            <UserButton/>
+            {
+                isTeacherPage || isPlayerPage ? (
+                    <Button size="sm" variant="ghost" className="hover:underline">
+                        <LogOut className="h-4 w-4 mr-2"/>
+                         Exit
+                    </Button>
+                ) : ( 
+                    <Link href="/teacher/courses">
+                        <Button size="sm" variant="ghost" className="hover:underline">
+                            Teacher Mode
+                        </Button>
+                    </Link>
+                )
+            }
+            <UserButton
+                afterSignOutUrl="/"
+            />
         </div>
     )
 }

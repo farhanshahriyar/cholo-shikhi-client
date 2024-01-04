81334b8b07ffed9611783ad6ca8d46a78e054ac0
farhanshahriyar <farhan.abir9999@gmail.com>
2024-01-04 20:28:54 +0600

    added custom icon-badge using typescript

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db"
 import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 
 const CourseIdPage = async ({
@@ -46,6 +48,14 @@ const CourseIdPage = async ({
                 <div className="flex flex-col gap-y-2">
                     <h1 className="text-2xl font-medium">Course Setup</h1>
                     <span className="text-slate-700 text-sm">Complete all fields {completionText} </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={LayoutDashboard}/>
+                        <h2 className="text-xl">Customize your course</h2>
+                    </div>
                 </div>
             </div>
          </div>

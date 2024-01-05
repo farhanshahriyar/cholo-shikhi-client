fedd4d04e3b0670652377aa2967f3f1847b162b1
farhanshahriyar <farhan.abir9999@gmail.com>
2024-01-05 22:35:45 +0600

    added formSchema

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -5,12 +5,31 @@ import axios from "axios"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 
+// develop ui for title form
+import {
+    Form,
+    FormField,
+    FormControl,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
 interface TitleFormProps {
     initialData: {
         title: string;
     }
     courseId: string;
 }
+
+// form schema
+const formSchema = z.object({
+    title: z.string().min(1,{
+        message: "Title is required"
+    }),
+})
 
 export const TitleForm = ({
     initialData,
